ec75f8c43fcbdcc0703b08e5203fab5fd0c9feb3
Julien Tanguy <tanguy.j@sfeir.com>
2025-10-02 18:26:28 +0200

    style: reformat steps with prettier

--- steps/ex02/nav.js
+++ steps/ex02/nav.js
@@ -1,6 +1,6 @@
-document.getElementById('home').addEventListener('click', function() {
-    location.href = 'index.html';
+document.getElementById('home').addEventListener('click', function () {
+  location.href = 'index.html';
 });
-document.getElementById('account').addEventListener('click', function() {
-    location.href = 'account.html';
+document.getElementById('account').addEventListener('click', function () {
+  location.href = 'account.html';
 });
